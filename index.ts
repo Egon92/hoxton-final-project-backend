17a963b66ef6d1a596a9cfb48c1a31ab9152c7c1
Egon92 <90727274+Egon92@users.noreply.github.com>
2022-04-12 15:16:43 +0200

    import dependencies

--- index.ts
+++ index.ts
@@ -1,8 +1,21 @@
 import express, { application } from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import "dotenv/config";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const prisma = new PrismaClient();
+
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server on http://localhost:${PORT}`);
+});
